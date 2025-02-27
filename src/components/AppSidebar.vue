06c241cd0fcb61a71e7202b25e8cd791b07c2469
nguyen083 <billbatri085@gmail.com>
2025-02-27 15:49:05 +0700

    chore : add dark mode

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -2,6 +2,7 @@
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -9,10 +10,15 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar'
+import { Switch } from '@/components/ui/switch'
+import { useColorMode } from '@vueuse/core'
 import { Home, Menu } from 'lucide-vue-next'
+import { ref } from 'vue'
 import { RouterLink, useRoute } from 'vue-router'
 
 const route = useRoute()
+const isDark = ref<boolean>(false)
+const mode = useColorMode()
 // Menu items.
 const items = [
   {
@@ -26,6 +32,11 @@ const items = [
     icon: Menu,
   },
 ]
+
+function changeTheme() {
+  isDark.value = !isDark.value
+  mode.value = mode.value === 'light' ? 'dark' : 'light'
+}
 </script>
 
 <template>
@@ -50,5 +61,13 @@ const items = [
         </SidebarGroupContent>
       </SidebarGroup>
     </SidebarContent>
+    <SidebarFooter class="flex justify-center items-center gap-2">
+      <Switch :model-value="isDark" @update:model-value="changeTheme">
+        <template #thumb>
+          <Icon v-if="isDark" icon="lucide:moon" class="size-3" />
+          <Icon v-else icon="lucide:sun" class="size-3" />
+        </template>
+      </Switch>
+    </SidebarFooter>
   </Sidebar>
 </template>
